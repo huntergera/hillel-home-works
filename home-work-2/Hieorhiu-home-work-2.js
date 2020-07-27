b47b318f7afe65d3f0eb0d3fdbc1d92891a98862
huntergera <huntergera91@gmail.com>
2020-07-27 08:05:06 +0300

    finished home work 2

--- home-work-2/Hieorhiu-home-work-2.js
+++ home-work-2/Hieorhiu-home-work-2.js
@@ -1,12 +1,13 @@
 // 1 task
-let userName = prompt('Как тебя зовут?' ),
-    userBirthYear = +prompt('Какого ты года рождения?'),
-    currentDate = new Date(),
-    userAge = currentDate.getFullYear() - userBirthYear,
-    promptNumbers = [];
+const userName = prompt('Как тебя зовут?' ),
+      userBirthYear = +prompt('Какого ты года рождения?'),
+      currentDate = new Date(),
+      userAge = currentDate.getFullYear() - userBirthYear;
+
+let promptNumbers = [];
 
 if(Number.isNaN(userBirthYear)) {
-    alert('Введите верный год рождения');
+    alert('Вы ввели неверный год рождения');
 } else {
     alert(`${userName} ${userAge}`);
 }
@@ -15,7 +16,7 @@ if(Number.isNaN(userBirthYear)) {
 promptNumbers[0] = +prompt('Введите первое число' );
 promptNumbers[1] = +prompt('Введите второе число' );
 promptNumbers[2] = +prompt('Введите третье число' );
-let additionResult = promptNumbers[0] + promptNumbers[1] + promptNumbers[2];
+const additionResult = promptNumbers[0] + promptNumbers[1] + promptNumbers[2];
 
 console.log(`Сумма трех чисел = ${additionResult}`);
 
@@ -30,12 +31,62 @@ console.log( `
 `);
 
 // 3 task
-let averageNumber = additionResult / promptNumbers.length;
+const averageNumber = additionResult / promptNumbers.length;
 console.log(`Среднее арифметическое = ${+averageNumber.toFixed(2)}`);
 
 // 4 task
 let bigNumber = 12345;
-let arr = bigNumber.toString().split('').join(' ');
-console.log(arr, typeof arr);
+
+const arr = bigNumber.toString().split('').reverse().join(' ');
+console.log(arr);
+
+  // Второй вариант
+const firstNumber = bigNumber % 10;
+bigNumber = (bigNumber - firstNumber) / 10;
+
+const secondNumber = (bigNumber % 10);
+bigNumber = (bigNumber - secondNumber) / 10;
+
+const thirdNumber = (bigNumber % 10);
+bigNumber = (bigNumber - thirdNumber) / 10;
+
+const fourthNumber = (bigNumber % 10);
+bigNumber = (bigNumber - fourthNumber) / 10;
+
+const fifthNumber = (bigNumber % 10);
+bigNumber = (bigNumber - fifthNumber) / 10;
+
+console.log(firstNumber, secondNumber, thirdNumber, fourthNumber, fifthNumber);
+
 
 // 5 task
+if(promptNumbers[0] > promptNumbers[1]){
+  console.log(`${promptNumbers[0]} > ${promptNumbers[1]}`);
+} else if(promptNumbers[0] < promptNumbers[1]) {
+  console.log(`${promptNumbers[0]} < ${promptNumbers[1]}`);
+} else {
+  console.log(`${promptNumbers[0]} = ${promptNumbers[1]}`);
+}
+
+// 6 task
+const randomNumber = 4567855,
+      lastNumber = randomNumber % 10;
+const lastNumberIsEven =  lastNumber % 2 === 0 ? `Цифра ${lastNumber} четная` : `Цифра ${lastNumber} нечетная`;
+console.log(lastNumberIsEven);
+
+// 7 task
+const currentUserName = prompt('Как тебя зовут?' ),
+      currentUserAge = +prompt('Сколько вам лет?' ),
+      alcoholDrinking = confirm("Алкоголь употребляем?");
+
+if(alcoholDrinking === true) {
+  if(currentUserAge > 40) {
+    alert(`${currentUserName}, не злоупотребляйте`)
+  } else if(currentUserAge < 18) {
+    alert(`${currentUserName}, ты что?! Маме расскажу!`)
+  } else {
+    alert(`${currentUserName}, только водку с пивом не мешай...`)
+  }
+} else {
+  alert(`${currentUserName}, так держать!`)
+}
