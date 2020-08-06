cf8998d127b664b00fa8421181a52b00a0032e74
Hieorhiu Yurii <j.hieorhiu@autodoc.eu>
2020-08-06 18:52:33 +0300

    added function at hw-6

--- home-work-6/Hieorhiu-home-work-6.js
+++ home-work-6/Hieorhiu-home-work-6.js
@@ -1,7 +1,29 @@
 'use strict';
 
+const numbersArray = [1, 2, 3, 4];
+
+// function forEach
 function forEach(array, callback) {
-    for(let item of array) {
-        return callback;
+    for (let i = 0; i < array.length; i++) {
+        callback(array[i]);
     }
-};
+}
+// example for function forEach
+forEach(numbersArray, (item) => {
+    console.log(item + 5);
+});
+
+let newNumbersArray = [];
+function map(array, callback) {
+    for (let i = 0; i < array.length; i++) {
+        callback(array[i]);
+        console.log(array[i])
+        newNumbersArray.push(array[i]);
+    }
+    return newNumbersArray;
+}
+
+map(numbersArray, (item) => {
+    return item + 5
+});
+console.log(newNumbersArray);
