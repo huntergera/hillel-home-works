086b442a14f9095e327786fd6d5e03d33ced2807
huntergera <huntergera91@gmail.com>
2020-08-13 07:59:51 +0300

    added tasks at hw-7

--- home-work-7/Hieorhiu-home-work-7.js
+++ home-work-7/Hieorhiu-home-work-7.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// objects
 const user = {
     id: 12,
     position: "Middle",
@@ -7,9 +8,11 @@ const user = {
 
 const userContacts = {
     email: "email@gmail.com",
+    slack: "nick",
     telegram: "@nick",
 };
 
+// 1 task
 function copy(target, origin) {
     for (const key in origin) {
         const value = origin[key];
@@ -19,5 +22,27 @@ function copy(target, origin) {
 }
 
 copy(user, userContacts);
-
 console.log(user);
+
+// 2 task
+function compare(object1, object2) {
+    if(Object.keys(object1) === Object.keys(object2)) {
+        return true
+        console.log(1)
+    }
+    console.log(2)
+    // const keys = Object.keys(user);
+    //
+    // const values = Object.values(user);
+}
+
+compare(user, userContacts);
+
+
+
+
+function main() {
+    copy(user, userContacts);
+}
+
+main();
