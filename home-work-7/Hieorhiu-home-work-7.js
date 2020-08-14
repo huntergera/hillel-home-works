224f3c22373478c65ff42723463bec7569eda50d
huntergera <huntergera91@gmail.com>
2020-08-14 08:18:40 +0300

    added tasks at hw-7

--- home-work-7/Hieorhiu-home-work-7.js
+++ home-work-7/Hieorhiu-home-work-7.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// objects
+// data
 const user = {
     id: 12,
     position: "Middle",
@@ -12,6 +12,22 @@ const userContacts = {
     telegram: "@nick",
 };
 
+const address1 = {
+    country: "UA",
+    city: "Odessa",
+    street: "Kanatnaya",
+    homeNumber: 22
+};
+
+const address2 = {
+    country: "UA",
+    city: "Odessa",
+    street: "Kanatnaya",
+    homeNumber: 22
+};
+
+const string = "aaabbc";
+
 // 1 task
 function copy(target, origin) {
     for (const key in origin) {
@@ -21,28 +37,47 @@ function copy(target, origin) {
     return target;
 }
 
-copy(user, userContacts);
-console.log(user);
-
 // 2 task
 function compare(object1, object2) {
-    if(Object.keys(object1) === Object.keys(object2)) {
+    const keysArray1 = Object.keys(object1);
+    const keysArray2 = Object.keys(object2);
+    const valuesArray1 = Object.values(object1);
+    const valuesArray2 = Object.values(object2);
+    if(keysArray1.length !== keysArray2.length) {
+        return false
+    } else {
+        for (let i = 0; i < keysArray1.length; i++) {
+            if (keysArray1[i] !== keysArray2[i]) {
+                return false
+            }
+        }
+        for (let i = 0; i < valuesArray1.length; i++) {
+            if (valuesArray1[i] !== valuesArray2[i]) {
+                return false
+            }
+        }
         return true
-        console.log(1)
     }
-    console.log(2)
-    // const keys = Object.keys(user);
-    //
-    // const values = Object.values(user);
 }
 
-compare(user, userContacts);
+//3 task
+function countItemsOfString(str) {
+    const result = {};
+    const chars = str.split("");
+    for (let i = 0; i < chars.length; i++) {
+        let count = result[chars[i]]? result[chars[i]] : 0;
+        result[chars[i]] = count + 1;
+    }
+    return result;
+}
 
 
 
 
 function main() {
-    copy(user, userContacts);
-}
+    console.log(copy(user, userContacts));
+    console.log(compare(user, userContacts));
+    console.log(compare(address1, address2));
 
+}
 main();
