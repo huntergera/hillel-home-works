8c2df18cb5a88374bbf56f3a173d767d7eccf4d0
Hieorhiu Yurii <j.hieorhiu@autodoc.eu>
2020-08-20 17:36:33 +0300

    finished hw-10

--- home-work-10/Hieorhiu-home-work-10.js
+++ home-work-10/Hieorhiu-home-work-10.js
@@ -1,10 +1,45 @@
 'use strict';
 const button = document.querySelectorAll("button")[0];
 
-function onClick() {
+function getCountOfElements(wrapper, element) {
+    const elements = document.querySelector(wrapper).querySelectorAll(element);
+    return elements.length;
+}
 
+const spanCount = getCountOfElements('#wrapper', 'span');
+const pCount = getCountOfElements('#wrapper', 'p');
+const allCount = getCountOfElements('#wrapper', '*');
+
+function getTypeOfValue(wrapper, element) {
+    const countElements = {
+        countString: 0,
+        countNumber: 0,
+    };
+
+    const allElements = document.querySelector(wrapper).querySelectorAll(element);
+    for (const item of allElements) {
+        if (isNaN(item.innerHTML)) {
+            countElements.countString += 1
+        } else {
+            countElements.countNumber += 1
+        }
+    }
+    return countElements;
+}
+
+const countAllElements = Object.assign({}, getTypeOfValue('#wrapper', '*'));getTypeOfValue('#wrapper', '*')
+
+function renderResultToElement(element) {
+    const result =  document.querySelector(element);
+    result.innerText = `
+        Количество всех тегов: ${allCount}
+        Количество тегов p: ${pCount}
+        Количество тегов span: ${spanCount}
+        Кол-во тегов, в которых содержимое - число: ${countAllElements.countNumber}
+        Кол-во тегов, в которых содержимое - строка: ${countAllElements.countString}
+    `;
 }
 
 if (button !== null) {
-    button.addEventListener("click", onClick);
+    button.addEventListener("click",()=>{renderResultToElement('#result')});
 }
