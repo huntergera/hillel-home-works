60d2bdffa54d224b83618ad867bab4ddfc59d561
Hieorhiu Yurii <j.hieorhiu@autodoc.eu>
2020-08-25 11:06:45 +0300

    fixed hw-10

--- home-work-10/Hieorhiu-home-work-10.js
+++ home-work-10/Hieorhiu-home-work-10.js
@@ -1,32 +1,32 @@
 'use strict';
 const button = document.querySelectorAll("button")[0];
+const wrapper = document.querySelector('#wrapper');
 
 function getCountOfElements(wrapper, element) {
-    const elements = document.querySelector(wrapper).querySelectorAll(element);
+    const elements = wrapper.querySelectorAll(element);
     return elements.length;
 }
-
-const spanCount = getCountOfElements('#wrapper', 'span');
-const pCount = getCountOfElements('#wrapper', 'p');
-const allCount = getCountOfElements('#wrapper', '*');
 
 function getTypeOfValue(wrapper, element) {
     const countElements = {
         countString: 0,
         countNumber: 0,
     };
-    const allElements = document.querySelector(wrapper).querySelectorAll(element);
+    const allElements = wrapper.querySelectorAll(element);
 
     for (const item of allElements) {
-        isNaN(item.innerHTML) ? countElements.countString += 1 : countElements.countNumber += 1
+        Number.isNaN(+item.innerHTML) ? countElements.countString += 1 : countElements.countNumber += 1
     }
     return countElements;
 }
 
-const countAllElements = Object.assign({}, getTypeOfValue('#wrapper', '*'));
-
 function renderResultToElement(element) {
     const result =  document.querySelector(element);
+    const spanCount = getCountOfElements(wrapper, 'span');
+    const pCount = getCountOfElements(wrapper, 'p');
+    const allCount = getCountOfElements(wrapper, '*');
+    const countAllElements = Object.assign({}, getTypeOfValue(wrapper, '*'));
+
     result.innerText = `
         Количество всех тегов: ${allCount}
         Количество тегов p: ${pCount}
