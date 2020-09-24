9856048da30448ca6bbe8d25f9ee7e87bf14e3a8
Hieorhiu Yurii <j.hieorhiu@autodoc.eu>
2020-09-24 18:58:08 +0300

    hw-15

--- home-work-15/Hieorhiu-home-work-15.js
+++ home-work-15/Hieorhiu-home-work-15.js
@@ -2,7 +2,7 @@
 
 
 const wrapper = document.querySelector("#wrapper");
-const colorsList = ["red", "green",]
+const colorsList = ["red", "green", "yellow", "blue"];
 
 if (wrapper !== null) {
     const container = document.createElement("div");
@@ -10,21 +10,45 @@ if (wrapper !== null) {
     wrapper.appendChild(container);
 
     setInterval(() => {
+        container.innerHtml = 'dfg';
+        console.log(container.innerHtml)
         const date = displayDate();
-        container.innerHTML = date;
-        console.log(date)
+        for (let item of date) {
+            container.appendChild(item)
+        }
+        console.log(container.innerHtml)
+        container.innerHtml = '';
+        //console.log(date)
+        //container.innerHTML = date;
+        // console.clear();
+        // console.log(date);
     }, 1000);
 
 }
 
 
 function displayDate() {
-
     const now = new Date();
     const time = localFormat(now);
-    console.log(typeof time)
+    const timeFormatted = [];
+    for (let item of time) {
+        //console.log(item)
+        const itemWrapper = document.createElement("span");
+        itemWrapper.className = "item-wrap";
 
-    return time;
+        itemWrapper.innerHTML = item;
+
+        timeFormatted.push(itemWrapper);
+        //console.log(timeFormatted)
+
+        //console.log(itemWrapper)
+
+        // const colorIndex = colorsList.findIndex(color => color === item.style.backgroundColor);
+        // colorIndex === colorsList.length - 1 || colorIndex === -1
+        //     ? item[i].style.backgroundColor = colorsList[0]
+        //     : item[i].style.backgroundColor = colorsList[colorIndex + 1];
+    }
+    return timeFormatted;
 }
 
 function usaFormat(date) {
