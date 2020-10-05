71be2c058a3af53da9d88d789bcab47c075ca93b
huntergera <huntergera91@gmail.com>
2020-10-05 07:47:12 +0300

    hw-18

--- home-work-18/Hieorhiu-home-work-18.js
+++ home-work-18/Hieorhiu-home-work-18.js
@@ -2,42 +2,15 @@
 const BASE_URL = "https://rickandmortyapi.com/api/character/";
 const wrapper = document.querySelector("#wrapper");
 
-// function loadCharacters(params, onSuccess) {
-//     const xhr = new XMLHttpRequest();
-//     xhr.responseType = "json";
-//
-//     const searchParams = new URLSearchParams();
-//     Object.keys(params)
-//         .forEach((key) => searchParams.set(key, params[key]));
-//
-//     xhr.open("GET", `${BASE_URL}?${searchParams}`);
-//
-//     xhr.onload = function () {
-//         if (xhr.status === 200) {
-//             onSuccess({
-//                 hasNextPage: xhr.response.info.next !== null,
-//                 hasPrevPage: xhr.response.info.prev !== null,
-//                 page: params.page,
-//                 results: xhr.response.results
-//             });
-//         } else {
-//             console.error("Something went wrong");
-//         }
-//     }
-//
-//     xhr.onerror = function () {
-//         console.error("ERROR");
-//     }
-//
-//     xhr.send();
-// }
-//
-// function render(data) {
-//     console.log("render", data);
-// }
-//
-// loadCharacters({ page: 1 }, render);
-// console.log("send");
+const buttonTypes = Object.freeze({
+    prev: "prev",
+    next: "next"
+});
+
+const keyCodes = Object.freeze({
+    arrowLeft: "ArrowLeft",
+    arrowRight: "ArrowRight"
+});
 
 class CharacterList {
     constructor() {
@@ -45,12 +18,24 @@ class CharacterList {
         this.data = {};
 
         this.list = document.createElement("ul");
+
+        this.prevButton = document.createElement("button");
+        this.prevButton.className = "waves-effect waves-light btn";
+        this.prevButton.innerText = "PREV";
+        this.prevButton.dataset.type = buttonTypes.prev;
+
+        this.nextButton = document.createElement("button");
+        this.nextButton.className = "waves-effect waves-light btn";
+        this.nextButton.innerText = "NEXT";
+        this.nextButton.dataset.type = buttonTypes.next;
+
+        this.pageLabel = document.createElement("span");
+        this.pageLabel.className = "page";
     }
 
     loadCharacters() {
         const xhr = new XMLHttpRequest();
         xhr.responseType = "json";
-
 
         const searchParams = new URLSearchParams();
         searchParams.set("page", this.page);
@@ -79,24 +64,73 @@ class CharacterList {
     }
 
     onDataLoad() {
-        console.log(this.data);
+        this.list.innerHTML = "";
+
+        for (const user of this.data.results) {
+            const listItem = document.createElement("li");
+            listItem.innerText = user.name;
+            this.list.appendChild(listItem);
+        }
+
+        this.pageLabel.innerText = this.page;
+
+        this.prevButton.disabled = !this.data.hasPrevPage;
+        this.nextButton.disabled = !this.data.hasNextPage;
     }
 
-    createListItem(props) {
-        const element = document.createElement("li");
-        element.innerHTML = text;
+    onButtonClick(event) {
+        console.log(event, this);
+        switch (event.target.dataset.type) {
+            case buttonTypes.next: {
+                this.page++;
+                this.loadCharacters();
+                break;
+            }
+            case buttonTypes.prev: {
+                this.page--;
+                this.loadCharacters();
+                break;
+            }
+            default: break;
+        }
     }
 
-    createList() {
-        const element = document.createElement("ul");
-        list.className = "users-list";
-
+    onKeyDown(event) {
+        switch (event.key) {
+            case keyCodes.arrowLeft: {
+                if (this.data.hasPrevPage) {
+                    this.page--;
+                    this.loadCharacters();
+                }
+                break;
+            }
+            case keyCodes.arrowRight: {
+                if (this.data.hasNextPage) {
+                    this.page++;
+                    this.loadCharacters();
+                }
+                break;
+            }
+            default: break;
+        }
     }
 
     render() {
-        console.log("CharacterList.render", this.page, this.data);
+        [
+            this.nextButton,
+            this.prevButton
+        ].forEach(button => button.addEventListener("click", this.onButtonClick.bind(this)));
+
+        document.addEventListener("keydown", this.onKeyDown.bind(this));
+
+        const paginatorWrapper = document.createElement("div");
+        paginatorWrapper.appendChild(this.prevButton);
+        paginatorWrapper.appendChild(this.pageLabel);
+        paginatorWrapper.appendChild(this.nextButton);
 
         wrapper.appendChild(this.list);
+        wrapper.appendChild(paginatorWrapper);
+
         this.loadCharacters();
     }
 }
