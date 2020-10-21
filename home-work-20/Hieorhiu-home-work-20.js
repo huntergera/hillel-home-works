e23de4ed610872a4ef32b559f7a582f36bfe0748
Hieorhiu Yurii <j.hieorhiu@autodoc.eu>
2020-10-21 12:01:00 +0300

    hw-20

--- home-work-20/Hieorhiu-home-work-20.js
+++ home-work-20/Hieorhiu-home-work-20.js
@@ -3,62 +3,106 @@ const wrapper = document.querySelector("#wrapper");
 const input = document.querySelector(".messenger__input");
 const form = document.querySelector("#form");
 const messagesList = document.querySelector(".messenger__message-list");
-const botMessages = [
-    "Ну что ж, до следующей встречи",
-    "Круто",
-    "Серьезно?",
-    "Так и знал",
-    "Это было предсказуемо",
-    "Не думаю",
-    "Все может быть",
-    "Не исключено",
-    "Это интересно",
-    "И что ты намереваешься сделать?",
-    "Ну это несерьезно"
-];
-const stopMessage = "ой все";
+const data = {
+    botIsActive: true,
+    firstBotMessage: `
+        Привет, я бот. Ты можешь задавать мне любые вопросы, а я постараюсь на них ответить. Также есть команды,с помощью которых ты можешь выполнять разные действия (прекратить / продолжить общение и т. д.), для этого введи "инфо"
+        `,
+    infoUserMessage: "инфо",
+    infoBotMessage: `
+            "ой все" - прекратить общение, <br>
+            "старт" - восстановить общение, <br>
+            "инфо" - вывести информацию о командах <br>
+        `,
+    startUserMessage: "старт",
+    startBotMessage: "и снова привет:)",
+    startBotMessageBotIsActive: "мы и так с тобой общаемся",
+    endUserMessage: "ой все",
+    endBotMessage: "Ну что ж, до следующей встречи",
+    botMessages: [
+        "Круто",
+        "Серьезно?",
+        "Так и знал",
+        "Это было предсказуемо",
+        "Не думаю",
+        "Все может быть",
+        "Не исключено",
+        "Это интересно",
+        "И что ты намереваешься сделать?",
+        "Ну это несерьезно",
+        "Лучше не торопиться в данном случае"
+    ]
+};
 
-form.addEventListener("submit", (event) => {
-    event.preventDefault();
+if (wrapper !== null) {
+    createBotMessage(data.firstBotMessage);
 
-    const inputValue = input.value;
-    if (inputValue) {
-        main(inputValue);
-    }
-});
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const inputValue = input.value;
+        if (inputValue) {
+            main(inputValue);
+        }
+    });
+}
 
 async function main(inputValue) {
     createUserMessage(inputValue);
-    const botMessageIndex = randomInteger(1, botMessages.length - 1);
-    await wait(1000);
-    //inputValue === stopMessage.toLowerCase() ? createBotMessage(botMessages[0]) :
-    if (inputValue === stopMessage.toLowerCase()) {
-        createBotMessage(botMessages[0]);
-        return false
+    const botMessageIndex = randomInteger(0, data.botMessages.length - 1);
+    const botMessageDelay = randomInteger(1, 3);
+    try {
+        await wait(botMessageDelay*1000);
+    } catch(err) {
+        throw new Error("Чтото пошло не так")
+    }
+
+    if (data.botIsActive === true) {
+        switch (inputValue) {
+            case data.endUserMessage.toLowerCase():
+                createBotMessage(data.endBotMessage);
+                data.botIsActive = false;
+                break;
+            case data.infoUserMessage.toLowerCase():
+                createBotMessage(data.infoBotMessage);
+                break;
+            case data.startUserMessage.toLowerCase():
+                createBotMessage(data.startBotMessageBotIsActive);
+                break;
+            default:
+                createBotMessage(data.botMessages[botMessageIndex]);
+        }
     } else {
-        createBotMessage(botMessages[botMessageIndex]);
+         if (inputValue === data.startUserMessage.toLowerCase()) {
+            createBotMessage(data.startBotMessage);
+            data.botIsActive = true;
+        }
     }
 }
 
+function createMessage(props) {
+    const message = document.createElement("li");
+    message.className = props.className;
+    message.innerHTML = props.text;
+
+    messagesList.appendChild(message);
+    message.scrollIntoView({block: "center", behavior: "smooth"});
+}
+
 function createUserMessage(text) {
-    const userMessage = document.createElement("li");
-    userMessage.className = "messenger__message messenger__message--user";
-    userMessage.innerHTML = text;
+    createMessage({
+        className: "messenger__message messenger__message--user",
+        text: text
+    });
 
     input.value = "";
     input.focus();
-
-    messagesList.appendChild(userMessage);
-    userMessage.scrollIntoView();
 }
 
 function createBotMessage(text) {
-    const botMessage = document.createElement("li");
-    botMessage.className = "messenger__message messenger__message--bot";
-    botMessage.innerHTML = text;
-
-    messagesList.appendChild(botMessage);
-    botMessage.scrollIntoView();
+    createMessage({
+        className: "messenger__message messenger__message--bot",
+        text: text
+    });
 }
 
 function randomInteger(min, max) {
