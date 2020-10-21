051931c7b666319c42cfd1cbd1070a9261876756
huntergera <huntergera91@gmail.com>
2020-10-21 07:57:23 +0300

    hw-20

--- home-work-20/Hieorhiu-home-work-20.js
+++ home-work-20/Hieorhiu-home-work-20.js
@@ -4,26 +4,40 @@ const input = document.querySelector(".messenger__input");
 const form = document.querySelector("#form");
 const messagesList = document.querySelector(".messenger__message-list");
 const botMessages = [
-    "Привет",
+    "Ну что ж, до следующей встречи",
     "Круто",
-    "Серьезно?"
+    "Серьезно?",
+    "Так и знал",
+    "Это было предсказуемо",
+    "Не думаю",
+    "Все может быть",
+    "Не исключено",
+    "Это интересно",
+    "И что ты намереваешься сделать?",
+    "Ну это несерьезно"
 ];
+const stopMessage = "ой все";
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
-    const inputValue = input.value;
 
-    if (input.value) {
-        createUserMessage(inputValue);
-        main();
+    const inputValue = input.value;
+    if (inputValue) {
+        main(inputValue);
     }
 });
 
-async function main() {
-
-    const botMessageIndex = await randomInteger(0, botMessages.length - 1);
-    await wait(1000)
-    await createBotMessage(botMessages[botMessageIndex]);
+async function main(inputValue) {
+    createUserMessage(inputValue);
+    const botMessageIndex = randomInteger(1, botMessages.length - 1);
+    await wait(1000);
+    //inputValue === stopMessage.toLowerCase() ? createBotMessage(botMessages[0]) :
+    if (inputValue === stopMessage.toLowerCase()) {
+        createBotMessage(botMessages[0]);
+        return false
+    } else {
+        createBotMessage(botMessages[botMessageIndex]);
+    }
 }
 
 function createUserMessage(text) {
@@ -32,18 +46,19 @@ function createUserMessage(text) {
     userMessage.innerHTML = text;
 
     input.value = "";
+    input.focus();
 
     messagesList.appendChild(userMessage);
+    userMessage.scrollIntoView();
 }
 
 function createBotMessage(text) {
-    const userMessage = document.createElement("li");
-    userMessage.className = "messenger__message messenger__message--bot";
-    userMessage.innerHTML = text;
+    const botMessage = document.createElement("li");
+    botMessage.className = "messenger__message messenger__message--bot";
+    botMessage.innerHTML = text;
 
-    input.value = "";
-
-    messagesList.appendChild(userMessage);
+    messagesList.appendChild(botMessage);
+    botMessage.scrollIntoView();
 }
 
 function randomInteger(min, max) {
